c9a13a51a427ba66ad4b19321e301f48a22fddc6
Christoph Brosch <christoph.brosch@outlook.de>
2021-10-16 13:46:43 +0200

    added apostroph

--- components/common/Modal.js
+++ components/common/Modal.js
@@ -35,7 +35,7 @@ export default class CustomModal extends React.Component {
                 <Modal.Header closeButton>
                 <Modal.Title>Modal heading</Modal.Title>
                 </Modal.Header>
-                <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+                <Modal.Body>Woohoo, you&apos;re reading this text in a modal!</Modal.Body>
                 <Modal.Footer>
                 <Button variant="secondary" onClick={this.hide}>
                     Close
