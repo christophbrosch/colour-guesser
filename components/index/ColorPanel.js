af85f044eb2ea129ef20f2582a52b956dff507e6
Christoph Brosch <christoph.brosch@outlook.de>
2021-10-16 19:11:01 +0200

    added colours.json and python script for parsing the source html form wikipedia

--- components/index/ColorPanel.js
+++
@@ -1,5 +0,0 @@
-export default function ColorPanel() {
-    return (
-        <div style={{backgroundColor: "red", width: "100%", height: "100%"}}></div>
-    )
-} 
--- pages/index.js
+++ pages/index.js
@@ -8,17 +8,40 @@ import DisplayRound from 'components/index/DisplayRound'
 import DisplayScore from 'components/index/DisplayScore'
 import DisplayBest from 'components/index/DisplayBest'
 
-import ColorPanel from 'components/index/ColorPanel'
+import ColourPanel from 'components/index/ColourPanel'
 import Choice from 'components/index/Choice'
 
-export default function Game() {
+import { getColourData } from 'lib/game'
+
+export default function Game({ colours }) {
   
+  const [colour, setColour] = useState("#eeeeee")
+
   const [score, setScore] = useState(0)
   const [time, setTime] = useState(20.0)
   const [round, setRound] = useState(0)
   const [best, setBest] = useState(0)
 
   const [choices, setChoices] = useState(Array(4).fill('default'))
+
+  
+  const randomlySetNewColour = function() {
+    let keys = Object.keys(colours)
+    const primaryColour = keys[keys.length * Math.random() << 0]
+
+    keys = Object.keys(colours[primaryColour])
+    const secondaryColour = keys[keys.length * Math.random() << 0]
+
+    const newColour = colours[primaryColour][secondaryColour]
+    setColour(newColour)
+
+    console.log('New colour:', newColour)
+  }
+
+  useEffect(() => {
+    const interval = setInterval(randomlySetNewColour, 10000)
+    return () => clearInterval(interval)
+  }, [])
 
   return (
     <Layout>
@@ -40,8 +63,8 @@ export default function Game() {
           <DisplayBest value={best}/>
         </Col>
       </Row>
-      <Row className="colorpanel">
-        <ColorPanel />
+      <Row className="colourpanel">
+        <ColourPanel colour={colour}/>
       </Row>
       <Row className="answer-area align-items-center">
         { choices.map( (choice, index) => {
@@ -53,5 +76,16 @@ export default function Game() {
         })}
       </Row>
     </Layout>
-    )
+  )
+}
+
+export async function getStaticProps() {
+  const colours = getColourData()
+
+  return {
+    props: {
+      colours: colours
+    }
   }
+}
+
